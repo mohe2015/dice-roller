4f61481c29ecd1a1a1ad51ca633e1292a499e907
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-23 16:32:09 +0200

    new idea

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,25 @@
+use crate::DieNumber::*;
 
+// just store the full cube and then simulate the rotation
 
 enum Direction {
     Up, Right, Down, Left
 }
 
+enum DieNumber {
+    One, Two, Three, Four, Five, Six
+}
+
+// front back top bottom left right
+struct Die([DieNumber; 6]);
+
 // x_position y_position top_dice_number front_dice_number
-fn move_dice(pos_and_state: (u8, u8, u8, u8), direction: Direction) -> (u8, u8, u8, u8) {
-     match direction {
-        Direction::Up => (pos_and_state.0, pos_and_state.1-1, pos_and_state.3, match pos_and_state.3 {
-           1 => 6,
-           2 => 5,
-           3 => 4,
-           4 => 3,
-           5 => 2,
-           6 => 1,
-           
-           /* State::OneTwo => State::TwoSix,
-            State::OneThree => State::ThreeSix,
-            State::OneFive => State::FiveSix,
-            State::OneFour => State::FourSix,
-
-            State::TwoOne => State::OneFive,
-            State::TwoFour => State::FourFive,
-            State::TwoSix => State::SixFive,
-            State::TwoThree => State::ThreeFive,
-
-            State::ThreeOne => State::OneFour,
-            State::ThreeTwo => State::TwoFour,
-            State::ThreeSix => State::SixFour,
-            State::ThreeFive => State::FiveFour,
-
-            State::FourOne => State::OneThree,
-            State::FourFive => State::FiveThree,
-            State::FourSix => State::SixThree,
-            State::FourTwo => State::TwoThree,
-
-            State::FiveOne => State::OneTwo,
-            State::FiveThree => State::ThreeTwo,
-            State::FiveSix => State::SixTwo,
-            State::FiveFour => State::FourTwo,
-
-            State::SixTwo => State::TwoOne,
-            State::SixFour => State::FourOne,
-            State::SixFive => State::FiveOne,
-            State::SixThree => State::ThreeOne,*/
-        }),
-        Direction::Right => (pos_and_state.0+1, pos_and_state.1, match pos_and_state.2 {
-            State::OneTwo => todo!(),
-            State::OneThree => todo!(),
-            State::OneFive => todo!(),
-            State::OneFour => todo!(),
-            State::TwoOne => todo!(),
-            State::TwoFour => todo!(),
-            State::TwoSix => todo!(),
-            State::TwoThree => todo!(),
-            State::ThreeOne => todo!(),
-            State::ThreeTwo => todo!(),
-            State::ThreeSix => todo!(),
-            State::ThreeFive => todo!(),
-            State::FourOne => todo!(),
-            State::FourFive => todo!(),
-            State::FourSix => todo!(),
-            State::FourTwo => todo!(),
-            State::FiveOne => todo!(),
-            State::FiveThree => todo!(),
-            State::FiveSix => todo!(),
-            State::FiveFour => todo!(),
-            State::SixTwo => todo!(),
-            State::SixFour => todo!(),
-            State::SixFive => todo!(),
-            State::SixThree => todo!(),
-        }),
-        Direction::Down => (pos_and_state.0, pos_and_state.1+1, match pos_and_state.2 {
-            State::OneTwo => todo!(),
-            State::OneThree => todo!(),
-            State::OneFive => todo!(),
-            State::OneFour => todo!(),
-            State::TwoOne => todo!(),
-            State::TwoFour => todo!(),
-            State::TwoSix => todo!(),
-            State::TwoThree => todo!(),
-            State::ThreeOne => todo!(),
-            State::ThreeTwo => todo!(),
-            State::ThreeSix => todo!(),
-            State::ThreeFive => todo!(),
-            State::FourOne => todo!(),
-            State::FourFive => todo!(),
-            State::FourSix => todo!(),
-            State::FourTwo => todo!(),
-            State::FiveOne => todo!(),
-            State::FiveThree => todo!(),
-            State::FiveSix => todo!(),
-            State::FiveFour => todo!(),
-            State::SixTwo => todo!(),
-            State::SixFour => todo!(),
-            State::SixFive => todo!(),
-            State::SixThree => todo!(),
-        }),
-        Direction::Left => (pos_and_state.0-1, pos_and_state.1,match pos_and_state.2 {
-            State::OneTwo => todo!(),
-            State::OneThree => todo!(),
-            State::OneFive => todo!(),
-            State::OneFour => todo!(),
-            State::TwoOne => todo!(),
-            State::TwoFour => todo!(),
-            State::TwoSix => todo!(),
-            State::TwoThree => todo!(),
-            State::ThreeOne => todo!(),
-            State::ThreeTwo => todo!(),
-            State::ThreeSix => todo!(),
-            State::ThreeFive => todo!(),
-            State::FourOne => todo!(),
-            State::FourFive => todo!(),
-            State::FourSix => todo!(),
-            State::FourTwo => todo!(),
-            State::FiveOne => todo!(),
-            State::FiveThree => todo!(),
-            State::FiveSix => todo!(),
-            State::FiveFour => todo!(),
-            State::SixTwo => todo!(),
-            State::SixFour => todo!(),
-            State::SixFive => todo!(),
-            State::SixThree => todo!(),
-        }),
-    }
+fn move_dice(pos_and_state: (u8, u8, Die), direction: Direction) -> (u8, u8, Die) {
+     
 }
 
 fn main() {
-    let pos_and_state: (u8, u8, u8, u8) = (0,0, 1, 2);
+    let pos_and_state: (u8, u8, Die) = (0,0, Die([One, Six, Four, Three, Two, Five]));
 
 
     println!("Hello, world!");
